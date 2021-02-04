c81a8bc2db865633eed13f5a6b9bf23994a41059
Ralvarez <R.alvgar@hotmail.com>
2021-02-04 16:46:55 +0100

    server socket.io listening

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
 require("dotenv").config();
 import express from "express";
 import { mqttService } from "./mqtt/mqttService";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+
+const httpServer = createServer();
+const io = new Server(httpServer, {});
+
+io.on("connection", (socket: Socket) => {
+  console.log("conected");
+
+  socket.on("message", (message) => {
+    console.log(message);
+    io.emit("message", message);
+  });
+});
+
+httpServer.listen(8080, () => console.log("listening"));
 
 const app = express();
-
-app.use(express.json());
+// const http = new Server(app)
+// const io = socket(http)
 
 app.listen(process.env.PORT, () => {
   mqttService.onConnect();
